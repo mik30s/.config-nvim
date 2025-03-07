7d7da228259e6a819bc68ecbf9b7801a0337eb0f
Michael Osei <3809183+mik30s@users.noreply.github.com>
2025-03-06 21:24:41 -0800

    Show hidden files

--- lua/mm/plugins/neotree.lua
+++ lua/mm/plugins/neotree.lua
@@ -9,5 +9,14 @@ return {
         "MunifTanjim/nui.nvim",
         -- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
     },
+    opts = {
+    filesystem = {
+      filtered_items = {
+        visible = true,
+        hide_dotfiles = false,
+        hide_gitignored = true,
+      }
+    }
+  }
 }
  
